42a677db6f3925f2b182903bd306e58a1670f77d
weizhibin <wzb19960208@outlook.com>
2018-02-06 10:57:58 +0800

    first

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const fs = require('fs');
+const chalk = require('chalk');
+
+http.createServer((req,res)=>{
+
+    fs.readFile('../page/index.html',(err,data)=>{
+
+        if(err){
+            console.error(err);
+        }
+
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.end(data);
+
+
+    });
+
+   
+    
+
+}).listen(9091,()=>{
+    console.info(chalk.green('Server started!'));
+});
+
