78a47cdb9d45525ca59b7eff44fe646509d7ad81
weizhibin <wzb19960208@outlook.com>
2018-02-07 16:37:27 +0800

    js

---
+++ websoc.js
@@ -0,0 +1,58 @@
+const net = require('net');
+const Transcoder = require('stream-transcoder');
+const chalk = require('chalk');
+const http = require('http');
+const fs = require('fs');
+
+const WebSocket = require('ws');
+ 
+const wss = new WebSocket.Server({ port: 8080 });
+ 
+// Broadcast to all.包括自己了
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  });
+};
+ 
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(data) {
+    // Broadcast to everyone else.
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
+});
+
+//tcp用于接收视频推流
+const tcpServer = net.createServer(function(sock){
+
+    let client = sock.remoteAddress+':'+sock.remotePort;
+
+    // 为'connection' 事件自动设置一个监听器。
+    console.info(chalk.green(client+' connect!'));
+
+    sock.on('data',function(data){
+        //console.info(data);
+        wss.broadcast(data);
+
+    })
+    
+
+    sock.on('close',function(){
+        console.info(chalk.green(client+' disconnect!'));
+    })
+
+    sock.on('error',function(err){
+        console.error(err);
+    })
+
+    
+
+
+}).listen(6002);
+
