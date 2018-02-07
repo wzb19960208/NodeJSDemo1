7b48a58523962518e0935ffddfa8c5907be48f61
weizhibin <wzb19960208@outlook.com>
2018-02-07 14:08:43 +0800

    image

--- socket.java
+++ socket.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import javax.imageio.ImageIO;
+import java.awt.Image;
 
 
 public class socket{
@@ -13,12 +15,18 @@ public class socket{
 		// TODO Auto-generated method stub
 		ServerSocket serverSocket = new ServerSocket(6002);
 		System.out.println("服务器启动!");
+		Image image;
 		// File file = new File("d:/screen2.png");
 		// FileOutputStream outputStream;
 		while(true){
 			Socket socket = null;
 			socket = serverSocket.accept();
 			System.out.println("new connection!");
+			image = ImageIO.read(socket.getInputStream());
+			
+			if(image!=null){
+				System.out.println("get image!");
+			}
 //			outputStream  = new FileOutputStream(file);
 //			byte[] data = readInputStream(socket.getInputStream());
 //			outputStream.write(data);
